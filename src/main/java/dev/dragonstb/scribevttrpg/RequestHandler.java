2efb0a0a89562817224c2b9ca10ab072149f87a7
Dragonstb <Dragonstb@users.noreply.github.com>
2023-11-11 19:57:41 +0100

    Added the personal hall webpage

--- src/main/java/dev/dragonstb/scribevttrpg/RequestHandler.java
+++ src/main/java/dev/dragonstb/scribevttrpg/RequestHandler.java
@@ -21,22 +21,6 @@ public class RequestHandler {
 
     @GetMapping("/")
     public String handleHome(@RequestHeader("Accept-Language") Locale loc, Model model) {
-//        // convert accepted languages to locale in a complicated way for now
-//        Locale loc = Locale.ENGLISH;
-//        if(langs != null) {
-//            String[] locCodes = langs.split(",");
-//            for (String locCode : locCodes) {
-//                // trim to first semicolon
-//                int semicolonAt = locCode.indexOf(";");
-//                if( semicolonAt > 0) {
-//                    locCode = locCode.substring(0, semicolonAt);
-//                }
-//                // get locale
-//                if( locCode.equals("de") || locCode.startsWith("de_") || locCode.startsWith("de-")) {
-//                    loc = Locale.GERMAN;
-//                }
-//            }
-//        }
 
         String mainTitle = messageSource.getMessage("web.mainTitle", null, "<web.mainTitle>", loc);
         model.addAttribute("mainTitle", mainTitle);
---
+++ src/main/resources/templates/scripts/hall.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', afterLoadingHall);
+
+function afterLoadingHall() {
+    console.log('now');
+    let hallSettingsButton = document.getElementById('hall-settings-btn');
+    let hallCampaignsButton = document.getElementById('hall-campaigns-btn');
+    let hallLogoutButton = document.getElementById('hall-logout-btn');
+    let hallSettingsPanel = document.getElementById('hall-settings-panel');
+    let hallCampaingsPanel = document.getElementById('hall-campaigns-panel');
+    let hallLogoutPanel = document.getElementById('hall-logout-panel');
+
+    function showHallSettingsPanel() {
+        hallSettingsPanel.classList.remove('nodisplay');
+        hallCampaingsPanel.classList.add('nodisplay');
+        hallLogoutPanel.classList.add('nodisplay');
+    }
+
+    function showHallCampaignsPanel() {
+        hallSettingsPanel.classList.add('nodisplay');
+        hallCampaingsPanel.classList.remove('nodisplay');
+        hallLogoutPanel.classList.add('nodisplay');
+    }
+
+    function showHallLogoutPanel() {
+        hallSettingsPanel.classList.add('nodisplay');
+        hallCampaingsPanel.classList.add('nodisplay');
+        hallLogoutPanel.classList.remove('nodisplay');
+    }
+
+    hallSettingsButton.addEventListener('click', showHallSettingsPanel);
+    hallCampaignsButton.addEventListener('click', showHallCampaignsPanel);
+    hallLogoutButton.addEventListener('click', showHallLogoutPanel);
+}
+

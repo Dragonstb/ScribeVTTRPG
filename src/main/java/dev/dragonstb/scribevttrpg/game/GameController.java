b81d539c0c64a24523991c4a319b110af839cf73
Dragonstb <Dragonstb@users.noreply.github.com>
2023-11-17 22:53:09 +0100

    implemented change from create session page to room on client side
    On the server, a minimal, hardcoded response is created. but on client
    side, the page can change to the newly created room now.

---
+++ src/main/java/dev/dragonstb/scribevttrpg/game/GameController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023, Dragonstb
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package dev.dragonstb.scribevttrpg.game;
+
+import dev.dragonstb.scribevttrpg.utils.Utils;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+/** The request controller for the game web page
+ *
+ * @author Dragonstb
+ * @since 0.0.3;
+ */
+@Controller
+public class GameController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/game/{*}")
+    public String getGamePage(@RequestHeader("Accept-Language") Locale loc, Model model) {
+
+        String baseTitle = messageSource.getMessage("web.common.baseTitle", null, "<web.common.baseTitle>", loc);
+        String campaignName="hello RPG world"; // TODO: fetch campaign title from the room associated with the name
+        String documentTitle = Utils.composeDocumentTitle(campaignName, baseTitle);
+
+        model.addAttribute("pageTitle", campaignName);
+        model.addAttribute("documentTitle", documentTitle);
+
+        return "game";
+    }
+
+}
---
+++ src/main/resources/static/scripts/create.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2023, Dragonstb
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+document.addEventListener('DOMContentLoaded', afterLoadingPage);
+
+function afterLoadingPage() {
+
+    let createBtn = document.querySelector('#create-session-btn');
+    createBtn.addEventListener('click', createSession);
+
+    function createSession() {
+        let target = createBtn.getAttribute('x-target');
+        if( target !== 'local') {
+            // get room name
+            let nameInput = document.querySelector('#roomName');
+            let roomName = null;
+            if(nameInput) {
+                roomName = nameInput.value;
+            }
+
+            // return and warn if roomName is null, too short, or too long, or uses inappropiate characters
+            // TODO: return and warn if roomName is null, too short, or too long, or uses inappropiate characters
+
+            // get campaign name
+            let campaign = document.querySelector('#campaignName').innerText;
+
+            // assemble request body
+            let body = {
+                campaign: campaign,
+                roomName: roomName
+            };
+
+            // TODO: turn on the aria-live="polite" waiting overlay that states "please wait" or similar
+            // send request
+            sendCreateGameRequest(target, body)
+                    .then(
+                            (resp) => {resolveCreateResponse(resp);}
+                    );
+
+        }
+        else {
+            // this is for local browsing, when testing the pages without connecting to the server
+            window.location.href="./game.html";
+        }
+    }
+
+    /** Sends the request for creating a new room for a new session
+     * @author Dragonstb
+     * @since 0.0.3;
+     * @param {string} url The URL the request is addressed to. Requires the campaign name and the room name as
+     * parameters. A parameter password is optional.
+     * @param {object} body The request body.
+     * @returns {object} The response object.
+     */
+    async function sendCreateGameRequest(url, body) {
+        const resp = await fetch(url, {
+            method: "POST",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+        return resp.json();
+    }
+
+    function resolveCreateResponse(data) {
+        // TODO: hide waiting overlay again
+        if(!data) {
+            console.log('no data');
+            return;
+        }
+
+        let validRoom = data.hasOwnProperty('room') && data.room && typeof data.room === 'string'
+                && data.room.length > 0;
+
+        if(validRoom) {
+            onCreateSuccess(data);
+        }
+        else {
+            onCreateError(data);
+        }
+    }
+
+    function onCreateError(data) {
+        // TODO: reflect error on screen
+        console.log('failed');
+        console.log(data);
+    }
+
+    function onCreateSuccess(data) {
+        window.location.href="./game/"+data.room;
+    }
+}
+
